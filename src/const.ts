6803d0c2bf61a36e32d42c31161cf469d4975ba7
canisminor1990 <i@canisminor.cc>
2024-01-20 02:10:01 +0800

    🐛 fix: Fix typo

--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,4 @@ import { resolve } from 'node:path';
 
 export const root = resolve(__dirname, '..');
 export const pluginDir = resolve(root, './public');
-export const BASE_URL = 'https://stack-overflow-search.onrender.com';
+export const BASE_URL = 'https://openai-collections.chat-plugin.lobehub.com';
